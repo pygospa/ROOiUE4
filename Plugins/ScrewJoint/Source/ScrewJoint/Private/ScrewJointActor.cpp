c073ffad4ee9722b1d67b8ab937c62d19c01b383
Kannan Thambiah <pygospa@gmail.com>
2018-04-01 09:24:25 +0200

    Initialize new plugin and create plugin class

---
+++ Plugins/ScrewJoint/Source/ScrewJoint/Private/ScrewJointActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ScrewJointActor.h"
+
+
+// Sets default values
+AScrewJointActor::AScrewJointActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AScrewJointActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScrewJointActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
