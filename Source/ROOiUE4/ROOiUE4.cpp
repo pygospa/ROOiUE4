796ad311230f06b9ceb3e995219da5f6f998de8c
Kannan Thambiah <pygospa@gmail.com>
2018-04-01 09:18:03 +0200

    Initialize project 'p3' as fresh new repository

---
+++ Source/ROOiUE4/ROOiUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ROOiUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ROOiUE4, "ROOiUE4" );
